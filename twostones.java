4325c3f73c84fa9886068628d4a25f7e32c41b3f
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-03-07 16:37:54 +0000

    Update twostones.java

--- twostones.java
+++ twostones.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class odd{
+public class twostones{
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         int x = scan.nextInt();
