0a57a29fa877a54cdfd55b65271a70c1e44f207d
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-03-07 16:36:17 +0000

    Create r2.java

---
+++ r2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class lol
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int r1 = scan.nextInt();
+        int s = scan.nextInt();
+        System.out.println((s*2)-r1);
+    }
+}
