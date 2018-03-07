681d99a810092c40caa6acf39726dcd002019390
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-03-07 16:35:42 +0000

    Create timeloop.java

---
+++ timeloop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class hi
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        for (int i = 1; i <= x; i++)
+        {
+            System.out.println(i + " " + "Abracadabra");
+        }
+    }
+}
