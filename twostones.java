69559e4539f9ab7fe4916928950f26d0b7185f08
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-03-07 16:34:02 +0000

    Create twostones.java

---
+++ twostones.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class odd{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        if (x%2==0){
+        System.out.println("Bob");
+        }
+        else{
+            System.out.println("Alice");
+        }
+    }
+}
