8875acebb4158d2832ac5a5b8e3049e65022a5a8
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-05-27 11:46:06 +0100

    Update skener.py

--- skener.py
+++ skener.py
@@ -3,6 +3,5 @@ for i in range (0,r):
   s = input()
   for j in range (0, zr):
     for k in range (0, len(s)):
-      for l in range (0,zc):
-        print(s[k], end="")
+      print(s[k]*zc, end="")
     print()
