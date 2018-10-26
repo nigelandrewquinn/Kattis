97615f9ef9ae8709ba31b49ba196f07716f8849d
Nigel Quinn <33383439+nigelandrewquinn@users.noreply.github.com>
2018-10-26 08:40:15 +0100

    Update shopaholic.py

--- shopaholic.py
+++ shopaholic.py
@@ -1,6 +1,1 @@
-input() 
-ans = 0 
-l = sorted(map(int,input().split()), reverse = True) 
-for i in range (2, len(l), 3):
-	ans += l[i]
-print(ans)
+input() ;print(sum(sorted(list(map(int, input().split())),reverse = True)[2::3]))  
